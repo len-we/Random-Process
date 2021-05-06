5d71b6ed5eb7640c39f37d5cacc6012eccd4cb25
XuYi-fei <857998989@qq.com>
2021-05-06 16:44:35 +0800

    对非齐次泊松的一点说明

--- PossionProcess/POSSION2.m
+++ PossionProcess/POSSION2.m
@@ -1,14 +1,14 @@
 %非齐次Poisson过程的模拟
-%时间为[0,8],密度lamda=1 lambda(t) = 8.924 - 1.584 * cos(pi * t / 1.51)
+%时间为[0,8],密度lamda=10 lambda(t) = 8.924 - 1.584 * cos(pi * t / 1.51)
 % + 7.897 * sin(pi * t / 3.02) - 10.434 * cos(pi * t / 4.53) + 4.293
 % * cos(pi * t / 6.04)
+clear;
 t=0;I=0;
 T=8;S=[];
-
 while t<=T
     U=rand(1,1);
     t=t-log(U);
-    lamda=1;
+    lamda=10;
     lamdat=lambda(t);
     if t<=T
         U1=rand(1,1);
